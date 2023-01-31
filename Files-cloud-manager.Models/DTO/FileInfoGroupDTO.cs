46055f2604872721b57106e1a62736a502f64255
prochuck <66011374+prochuck@users.noreply.github.com>
2023-01-31 19:12:28 +0300

    Добавлены DTO.
    Для FileInfo и FileInfoGroup.

---
+++ Files-cloud-manager.Models/DTO/FileInfoGroupDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files_cloud_manager.Models.DTO
+{
+    public class FileInfoGroupDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
