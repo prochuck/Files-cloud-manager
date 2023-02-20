71fdb6e274bb16df5b062a9619df1d4093f96f24
prochuck <66011374+prochuck@users.noreply.github.com>
2023-02-20 17:15:16 +0300

    Создана модель файла.

---
+++ Files-cloud-manager.Client/Models/ProgramDataModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files_cloud_manager.Client.Models
+{
+    class ProgramDataModel
+    {
+        public string PathToExe { get; set; }
+        public string PathToData { get; set; }
+
+
+        private string GetFullDataPath(string relativePath)
+        {
+            return $"{PathToData}/{relativePath}";
+        }
+    }
+}
