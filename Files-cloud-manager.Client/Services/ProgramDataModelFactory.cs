2a858860c92770b6467f85cd4418083aa343e33e
prochuck <66011374+prochuck@users.noreply.github.com>
2023-03-06 17:13:23 +0300

    Добавлена фабрика programDataModel

---
+++ Files-cloud-manager.Client/Services/ProgramDataModelFactory.cs
@@ -0,0 +1,35 @@
+﻿using Files_cloud_manager.Client.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files_cloud_manager.Client.Services
+{
+    internal class ProgramDataModelFactory
+    {
+        IServiceProvider _serviceProvider;
+
+        public ProgramDataModelFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public ProgramDataModel CreateProgramDataModelFactory(string login, string password)
+        {
+            var connectionService = _serviceProvider.GetRequiredService<ServerConnectionService>();
+            bool isLoginSuc = connectionService.LoginAsync(login, password).Result;
+
+            if (!isLoginSuc)
+            {
+                return null;
+            }
+
+            var res = new ProgramDataModel(connectionService, _serviceProvider.GetRequiredService<FileHashCheckerService>());
+
+            return res;
+        }
+    }
+}
